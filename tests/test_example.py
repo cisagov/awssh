27b8dd8cce7cff9a3d16e0bda5bf71e8753d23dd
David Redmin <david.redmin@trio.dhs.gov>
2022-03-10 00:05:40 -0500

    Make basic tests work and remove unused tests from skeleton

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env pytest -vs
-"""Tests for example."""
+"""Tests for awssh."""
 
 # Standard Python Libraries
 import logging
@@ -8,15 +8,10 @@ import sys
 from unittest.mock import patch
 
 # Third-Party Libraries
-import example
 import pytest
 
-div_params = [
-    (1, 1, 1),
-    (2, 2, 1),
-    (0, 1, 0),
-    (8, 2, 4),
-]
+# cisagov Libraries
+from awssh import awssh
 
 log_levels = (
     "debug",
@@ -28,14 +23,14 @@ log_levels = (
 
 # define sources of version strings
 RELEASE_TAG = os.getenv("RELEASE_TAG")
-PROJECT_VERSION = example.__version__
+PROJECT_VERSION = awssh.__version__
 
 
 def test_stdout_version(capsys):
     """Verify that version string sent to stdout agrees with the module version."""
     with pytest.raises(SystemExit):
         with patch.object(sys, "argv", ["bogus", "--version"]):
-            example.example.main()
+            awssh.main()
     captured = capsys.readouterr()
     assert (
         captured.out == f"{PROJECT_VERSION}\n"
@@ -51,8 +46,8 @@ def test_running_as_module(capsys):
             # being done by __main__ is importing the main entrypoint of the
             # package and running it, so there is nothing to use from this
             # import. As a result, we can safely ignore this warning.
-            # Third-Party Libraries
-            import example.__main__  # noqa: F401
+            # cisagov Libraries
+            import awssh.__main__  # noqa: F401
     captured = capsys.readouterr()
     assert (
         captured.out == f"{PROJECT_VERSION}\n"
@@ -79,7 +74,7 @@ def test_log_levels(level):
             ), "root logger should not have handlers yet"
             return_code = None
             try:
-                example.example.main()
+                awssh.main()
             except SystemExit as sys_exit:
                 return_code = sys_exit.code
             assert return_code is None, "main() should return success"
@@ -97,46 +92,7 @@ def test_bad_log_level():
     with patch.object(sys, "argv", ["bogus", "--log-level=emergency", "1", "1"]):
         return_code = None
         try:
-            example.example.main()
-        except SystemExit as sys_exit:
-            return_code = sys_exit.code
-        assert return_code == 1, "main() should exit with error"
-
-
-@pytest.mark.parametrize("dividend, divisor, quotient", div_params)
-def test_division(dividend, divisor, quotient):
-    """Verify division results."""
-    result = example.example_div(dividend, divisor)
-    assert result == quotient, "result should equal quotient"
-
-
-@pytest.mark.slow
-def test_slow_division():
-    """Example of using a custom marker.
-
-    This test will only be run if --runslow is passed to pytest.
-    Look in conftest.py to see how this is implemented.
-    """
-    # Standard Python Libraries
-    import time
-
-    result = example.example_div(256, 16)
-    time.sleep(4)
-    assert result == 16, "result should equal be 16"
-
-
-def test_zero_division():
-    """Verify that division by zero throws the correct exception."""
-    with pytest.raises(ZeroDivisionError):
-        example.example_div(1, 0)
-
-
-def test_zero_divisor_argument():
-    """Verify that a divisor of zero is handled as expected."""
-    with patch.object(sys, "argv", ["bogus", "1", "0"]):
-        return_code = None
-        try:
-            example.example.main()
+            awssh.main()
         except SystemExit as sys_exit:
             return_code = sys_exit.code
         assert return_code == 1, "main() should exit with error"
