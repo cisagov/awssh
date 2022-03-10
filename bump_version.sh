104891674ad1cbbf18b8154faee6247da130ebe4
David Redmin <david.redmin@trio.dhs.gov>
2022-03-09 23:02:54 -0500

    Update old references to renamed module

--- bump_version.sh
+++ bump_version.sh
@@ -6,7 +6,7 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
-VERSION_FILE=src/ssm_ssh/_version.py
+VERSION_FILE=src/awssh/_version.py
 
 HELP_INFORMATION="bump_version.sh (show|major|minor|patch|prerelease|build|finalize)"
 
