782d54fa0c3b57386fc7b4860113d6e0959e3bc1
Jason Odoom <jason.odoom@cisa.dhs.gov>
2023-09-08 15:41:19 -0400

    Add sed logic to finalize for flake version update

--- bump_version.sh
+++ bump_version.sh
@@ -46,9 +46,12 @@ else
       # A temp file is used to provide compatability with macOS development
       # as a result of macOS using the BSD version of sed
       tmp_file=/tmp/version.$$
+      tmp_flake=/tmp/flake.tmp
       sed "s/$old_version_regex/$new_version/" $VERSION_FILE > $tmp_file
       mv $tmp_file $VERSION_FILE
-      git add $VERSION_FILE
+      sed "s/$old_version_regex/$new_version/" $FLAKE_FILE > $tmp_flake
+      mv $tmp_flake $FLAKE_FILE
+      git add $FLAKE_FILE $FLAKE_LOCK_FILE $VERSION_FILE
       git commit -m"Finalize version from $old_version to $new_version"
       git push
       ;;
